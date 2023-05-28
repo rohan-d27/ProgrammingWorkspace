071415bc9513c3efb97dc56581c64d154e394ee3
Rohan <userzero00000@gmail.com>
2023-05-28 11:20:16 +0530

    work in progress

---
+++ ReactJs/simple-crud-app/src/HomePage.js
@@ -0,0 +1,15 @@
+// @flow
+import * as React from 'react';
+
+export class HomePage extends React.Component {
+    render() {
+        return (
+
+            <div>
+                <h1>Welcome to my Website!</h1>
+                <p>Here you can find all sorts of amazing things</p>
+                { /*Add your own content and components here*/}
+            </div>
+        );
+    };
+};
